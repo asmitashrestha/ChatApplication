1e3cb4ff0a0a40b4dfe65299d6c105ca39ca9536
Your Name <you@example.com>
2023-11-01 12:09:37 +0545

    data fetched

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,30 @@
 const express = require('express')
-const path = require('path')
+const dotenv = require('dotenv')
+const chats = require('./data/data')
+
 const app = express()
+dotenv.config()
+
+const PORT = process.env.PoRT || 5000
 
 app.get('/',(req,res)=>{
     res.send('API is running')
 })
 
-const server = app.listen(5000,()=>console.log(`🗨️ servers on port 5000`))
 
-app.use(express.static(path.join(__dirname,'public')))
+app.get('/chats',(req,res)=>{
+    res.send(chats)
+})
+
+app.get('/chats/:chatName',(req,res) =>{
+    const singleChat = chats.find((m) => m.chatName=== req.params.chatName);
+    if (singleChat) {
+        res.send(singleChat);
+    } else {
+        res.status(404).send('Chat not found');
+    }
+
+})
+
+const server = app.listen(5000,()=>console.log(`🗨️ servers on port ${PORT}`))
+
