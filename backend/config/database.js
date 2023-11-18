edc4935baec0908364cc591e11c684af03551e02
Your Name <you@example.com>
2023-11-18 08:22:34 +0545

    login api fixed

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/asmita')
+mongoose.connect('mongodb://127.0.0.1:27017/chatapp')
   .then(() => console.log('DB Connected!'))
   .catch(err=>console.log(err))
--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -9,52 +9,84 @@ const Home = () => {
   const [email,setEmail] = useState()
   const [password,setPassword] = useState()
   const [confirmpassword,seConfirmpassword] = useState()
-  const [img,seImg] = useState()
+  const [img,setImg] = useState()
   const [error, setError] = useState("")
   const [display,setDisplay] = useState(false)
+  const [loading,setLoading] = useState(false)
   const navigate = useNavigate()
 
   const handleClick = ()=> setDisplay(!display);
 
-  const postDetails = (imgs)=>{
-
-  }
-  const submitHandler = () =>{
-
-  }
-
-  const handleSubmit = (e) => {
-    
+  const postDetails = (imgs) => {
+    setLoading(true);
+    if (imgs === undefined) {
+      toast.warning("Please select an image!");
+      return;
+    }
+  
+    if (imgs.type === 'image/jpeg' || imgs.type === 'image/png') {
+      const data = new FormData();
+      data.append("file", imgs);
+      data.append("upload_preset", "chatapp"); 
+      data.append("cloud_name", "dkibeslb7");
+  
+      fetch("https://api.cloudinary.com/v1_1/dkibeslb7/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("Image URL:", data.url);
+          setImg(data.url.toString());
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast.warning("Please select an image!");
+    }
+  };
+  
+  const submitHandler = async (e) =>{
     e.preventDefault()
-    console.log('api called')
+    setLoading(true)
+    if(!name || !email || !password || !confirmpassword){
+      toast.warning("Please Fill all the fileds!")
+      setLoading(false)
+      return
+    }
+    if(password !== confirmpassword){
+      toast.warning("Passwords do not match")
+      return
+    }
 
-    fetch('https://fakestoreapi.com/auth/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        // username: "mor_2314",
-        // password: "83r5^_"
-        "username": e.target.name.value,
-        "password": e.target.password.value, 
-
+    try{
+      const { data } = await fetch("http://localhost:8000/user", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password, img }),
       })
-    })
-      .then(res =>{
-        if(res.ok){
-          toast.success("Login sucessful")
-          navigate('/')
-        }else{
-          toast.error("Invalid Credentials")
-        }
-      })
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-        console.error("Error ocurred during login")
-        toast.error("something went wrong -> mostly when interruption in internet")
-      })
+        .then((res) => res.json())
+        .catch((error) => {
+          console.error("Error during registration:", error);
+          throw error; // rethrow the error to be caught by the error handling below
+        });
+      
+      toast.success("Registration Successful")
+      localStorage.setItem("userInfo", JSON.stringify(data))
+      setLoading(false)
+      navigate("/chats")
+    }
+    catch(error){
+      toast.error(error.res.data.message)
+      setLoading(false)
+    }
   }
+
 
   return (
     <>
@@ -77,7 +109,7 @@ const Home = () => {
 
 
           <div className="mt-1 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={handleSubmit}>
+            <form className="space-y-6" >
               <div>
                 <label htmlFor="name" className="block text-md font-medium leading-6 text-gray-700 font-serif hover:text-gray-900">Name</label>
                 <div className="mt-1">
@@ -140,7 +172,7 @@ const Home = () => {
 
               <div className='flex relative bottom-5'>
                 <button type="submit" className="flex w-6/12 mr-2 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={submitHandler}>Signup</button>
+                onClick={submitHandler} disabled ={loading}>Signup</button>
                 <Link to='/' className='w-6/12 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"'>Login</Link>
                </div>
             </form>
