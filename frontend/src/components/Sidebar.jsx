ea44f12a1ace216a27657b29be78698cd63d3ac2
Your Name <you@example.com>
2023-11-22 10:05:08 +0545

    header ui for chat

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaSearch } from "react-icons/fa";
+import { IoMdNotifications } from "react-icons/io";
+import { ChatState } from "../Context/ChatProvider";
 
 const Sidebar = () => {
+  const [search, setSearch] = useState("")
+  const [searchResult, setSearchResult] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [loadingChat, setLoadingChat] = useState()
+  const { user } = ChatState()
   return (
-    <div>
-      <h1>sidebar</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae inventore beatae minima!</p>
+    <div className='boxcontainer '>
+      <div className="search-bar ml-11 mt-5">
+      <button className='flex'>
+        <input className='inp-des  outline-none ' type="text" placeholder="Search chats" />
+        <div className="search-box">
+          <FaSearch className='search'/>
+        </div>
+        
+      </button>
+    </div>
+    <div className="userdetail">
+      <div className="title-chats justify-center text-center text-2xl ">
+        <p >Chit-Chat</p>
+      </div>
+
+      <div className="tit-chat flex space-between">
+        <div className="tit-left mr-8">
+        <IoMdNotifications className='h-8 w-7' />
+        </div>
+        <div className="tit-left">
+        <img src={user.img} name={user.name} alt="user profile" height='30px' width='35px' className='img-user' />
+        </div>
+      </div>
+    </div>
+    <hr className='line'/>
     </div>
   )
 }
