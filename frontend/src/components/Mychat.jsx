c77113c17ff83dd1f820acc2eaeba37bfde11584
Your Name <you@example.com>
2023-11-27 12:03:39 +0545

    user search

--- frontend/src/components/Mychat.jsx
+++ frontend/src/components/Mychat.jsx
@@ -1,20 +1,19 @@
 import { Box, Stack, Text } from "@chakra-ui/layout";
 import { FaPlus } from "react-icons/fa";
-import { useToast } from "@chakra-ui/toast";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { getUser } from "../components/ChatLogics";
 import ChatLoading from "../components/Chatloading";
-import GroupChat from "../components/Groupchat";
-import { Button } from "@chakra-ui/react";
 import { ChatState } from "../Context/ChatProvider";
 
 const MyChats = ({ fetchAgain }) => {
   const [loggedUser, setLoggedUser] = useState();
 
   const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
+  const isSelected = selectedChat 
 
-  const toast = useToast();
 
   const fetchChats = async () => {
     // console.log(user._id);
@@ -39,81 +38,50 @@ const MyChats = ({ fetchAgain }) => {
   }, [fetchAgain]);
 
   return (
-    <Box
-      d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
-      flexDir="column"
-      alignItems="center"
-      p={3}
-      bg="white"
-      w={{ base: "100%", md: "31%" }}
-      borderRadius="lg"
-      borderWidth="1px"
-    >
-      <Box
-        pb={3}
-        px={3}
-        fontSize={{ base: "28px", md: "30px" }}
-        fontFamily="Work sans"
-        d="flex"
-        w="100%"
-        justifyContent="space-between"
-        alignItems="center"
+    <div className={`flex flex-col  p-3 bg-white rounded-lg border ${selectedChat ? 'hidden' : 'flex'} md:flex`}>
+      <div
+       className="text-xl mt-2"
       >
         My Chats
-        <GroupChat>
-          <Button
-            d="flex"
-            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
-            // rightIcon={<FaPlus/>}
+        <div>
+          <button className="flex  relative start-32 bottom-14 mt-7 p-1  text-xl bold hover:text-white hover:bg-blue-400 rounded-lg mt-4"
           >
-            New Group Chat
-          </Button>
-        </GroupChat>
-      </Box>
-      <Box
-        d="flex"
-        flexDir="column"
-        p={3}
-        bg="#F8F8F8"
-        w="100%"
-        h="100%"
-        borderRadius="lg"
-        overflowY="hidden"
-      >
+            New Group Chat<FaPlus className="ml-2 mt-1 mr-2"/>
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col bg-white width-full height-full rounded-lg overflow-hidden">
         {chats ? (
-          <Stack overflowY="scroll">
+          <div className="overflow-y-scroll">
             {chats.map((chat) => (
-              <Box
+              <div
                 onClick={() => setSelectedChat(chat)}
-                cursor="pointer"
-                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                color={selectedChat === chat ? "white" : "black"}
-                px={3}
-                py={2}
-                borderRadius="lg"
+                className={`cursor-pointer mt-1 w-80 px-3 py-2 rounded-lg ${
+                  isSelected ? "bg-blue-400 text-white" : "bg-gray-200 text-black"
+                }`}
                 key={chat._id}
               >
-                <Text>
+                <p>
                   {!chat.isGroupChat
                     ? getUser(loggedUser, chat.users)
                     : chat.chatName}
-                </Text>
+                </p>
                 {chat.newMessage && (
-                  <Text fontSize="xs">
-                    <b>{chat.newMessage.sender.name} : </b>
+                  <div className="text-xs">
+                    <p>{chat.newMessage.sender.name} : </p>
                     {chat.newMessage.content.length > 50
                       ? chat.newMessage.content.substring(0, 51) + "..."
                       : chat.newMessage.content}
-                  </Text>
+                  </div>
                 )}
-              </Box>
+              </div>
             ))}
-          </Stack>
+          </div>
         ) : (
           <ChatLoading />
         )}
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 };
 
